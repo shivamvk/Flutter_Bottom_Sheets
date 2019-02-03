276ce227539a13665259178f4d400619ee7291dd
shivamvk <shivam.vk529@gmail.com>
2019-02-03 21:55:34 +0530

    initial commit

---
+++ lib/flutter_bottom_sheet.dart
@@ -0,0 +1,18 @@
+library flutter_bottom_sheet;
+
+import 'package:flutter/material.dart';
+
+class FlutterBottomSheet extends StatefulWidget{
+
+
+  @override
+  _FlutterBottomSheetState createState() => _FlutterBottomSheetState();
+}
+
+class _FlutterBottomSheetState extends State<FlutterBottomSheet>{
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
---
+++ test/flutter_bottom_sheet_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_bottom_sheet/flutter_bottom_sheet.dart';
+
+void main() {
+
+}
