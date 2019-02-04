66d27d9c0194695e3771430e0f54cb3dfb5c4d67
shivamvk <shivam.vk529@gmail.com>
2019-02-04 16:41:21 +0530

    format flutter_bottom_sheet.dart

--- lib/flutter_bottom_sheet.dart
+++ lib/flutter_bottom_sheet.dart
@@ -2,30 +2,28 @@ library flutter_bottom_sheet;
 
 import 'package:flutter/material.dart';
 
-class FlutterBottomSheet extends StatefulWidget{
-
+class FlutterBottomSheet extends StatefulWidget {
   final String type;
   final BuildContext context;
   final Widget widget;
 
-  FlutterBottomSheet({
-    this.type,
-    this.context,
-    this.widget
-  });
+  FlutterBottomSheet({this.type, this.context, this.widget});
 
   @override
   _FlutterBottomSheetState createState() => _FlutterBottomSheetState();
 }
 
-class _FlutterBottomSheetState extends State<FlutterBottomSheet>{
+class _FlutterBottomSheetState extends State<FlutterBottomSheet> {
   @override
   Widget build(BuildContext context) {
     return new RaisedButton(
       onPressed: () {
-        showModalBottomSheet(context: widget.context, builder: (builder){ return widget.widget; });
+        showModalBottomSheet(
+            context: widget.context,
+            builder: (builder) {
+              return widget.widget;
+            });
       },
     );
   }
-
 }
